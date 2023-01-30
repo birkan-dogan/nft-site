129874ce9a7e3884d2db0144fb4e64b4f7bb88ca
birkan <b.561birkan@gmail.com>
2023-01-30 13:55:46 +0300

    installing packages, images and integrating cdns to run the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "../src/assets/icons/font-awesome.css";
+import "./App.css";
+import Home from "./pages/Home";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import AOS from "aos";
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 2000,
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Home />
     </div>
   );
-}
+};
 
 export default App;
