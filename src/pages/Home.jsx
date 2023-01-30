63c1b52ac60ecd1006e1ca7fb73e5b21467d0b33
birkan <b.561birkan@gmail.com>
2023-01-30 22:13:15 +0300

    working on Slider component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Header from "../components/header/Header";
+import Slider from "../components/slider/Slider";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <Slider />
     </div>
   );
 };
